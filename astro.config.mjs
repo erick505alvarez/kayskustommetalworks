c5b418a2fa4c3fa45f2c098d1e8307f9e0254296
Erick Alvarez <erick505alvarez@gmail.com>
2025-04-16 19:22:10 -0500

    Fix dynamic import for images. Convert React components to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+// astro.config.mjs
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,15 @@
+// src/content/config.ts
 import { defineCollection, z } from "astro:content";
 
 const modelsCollection = defineCollection({
   type: "data",
   schema: z.object({
     title: z.string(),
+    slug: z.string(),
     description: z.string(),
     parts: z.array(
       z.object({
-        image: z.string(),
+        imagePath: z.string(), // absolute path from root. Ex: /src/assets/imgs/*.webp
         name: z.string(),
         description: z.string().optional(),
       })
